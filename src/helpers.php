c95f948418c208d47987c75826d43bf25dde131a
linganmin <saboran@163.com>
2020-03-07 23:14:58 +0800

    feat: add function

--- src/helpers.php
+++ src/helpers.php
@@ -9,3 +9,14 @@ if (!function_exists('dingNotice')) {
         return app('dingtalk-notice')->useRobot()->getInstance();
     }
 }
+
+
+if (!function_exists('ding_notice')) {
+    function ding_notice($robot = '')
+    {
+        if ($robot) {
+            return app('dingtalk-notice')->useRobot($robot)->getInstance();
+        }
+        return app('dingtalk-notice')->useRobot()->getInstance();
+    }
+}
